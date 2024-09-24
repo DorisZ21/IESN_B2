c35c74ed49e8ce429ecce7cdfe0c140da0d5465e
DorisZ21 <etu54288@henallux.be>
2024-09-24 14:38:55 +0200

    ajout du cours de base de données

---
+++ Technologies Web/Labos/Labo 1/js/script.js
@@ -0,0 +1,8 @@
+let maintenant = new Date();
+let h = maintenant.getHours();
+let m = maintenant.getMinutes();
+
+
+function changeDyn(txt){
+    let cible = document.getElementById("dyna").innerHTML = txt;
+}
